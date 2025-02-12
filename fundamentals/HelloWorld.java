577af2b57c011c1ef537edeb15c1cded71542b03
Patrick F. <patrick.frueh@gmx.net>
2025-02-12 17:55:20 +0100

    Use upper case function to capitalize name

--- fundamentals/HelloWorld.java
+++ fundamentals/HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld {
     // The main method – the entry point of a Java application
     public static void main(String[] args) {
         System.out.println("Hello, World!"); // Print output
+
+        String name = "Peter";
+        String upperName = name.toUpperCase();
+        System.out.println(upperName); // Print output
+
     }
 }
