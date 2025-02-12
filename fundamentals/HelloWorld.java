6911dfb8b756bc47880debb94deee76a5599a00a
Patrick F. <patrick.frueh@gmx.net>
2025-02-12 17:19:59 +0100

    Print hello world

---
+++ fundamentals/HelloWorld.java
@@ -0,0 +1,7 @@
+// Define a class named HelloWorld
+public class HelloWorld {
+    // The main method – the entry point of a Java application
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Print output
+    }
+}
