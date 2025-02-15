5083601bbca8058c922c3c3c33ef168d0275c735
Patrick F. <patrick.frueh@gmx.net>
2025-02-15 16:33:43 +0100

    Add basic conditional example

---
+++ fundamentals/conditionals/Conditions.java
@@ -0,0 +1,13 @@
+public class Conditions {
+
+    public static boolean value = true;
+
+    public static void main (String args[]) {
+        if (value == true) {
+            System.out.println("real");
+        }
+        else {
+            System.out.println("no");
+        }
+    }
+}
