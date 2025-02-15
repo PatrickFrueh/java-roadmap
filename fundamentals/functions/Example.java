26698d77e742c28b28659dedef80825c1b8a589d
Patrick F. <patrick.frueh@gmx.net>
2025-02-15 17:05:30 +0100

    Add basic sum example

---
+++ fundamentals/functions/Example.java
@@ -0,0 +1,13 @@
+public class Example {  
+    // Method definition  
+    public static int add(int a, int b) {  
+        int sum = a + b; // Method body  
+        return sum; // Return statement  
+    }  
+
+    public static void main (String args[]) {
+        int added_numbers = add(5, 6);
+        System.out.println(added_numbers);
+
+    }
+}  
