b7e85f5489d72333148565d399c1409286242cc8
Patrick F. <patrick.frueh@gmx.net>
2025-03-05 15:46:05 +0100

    Manipulate string literal

---
+++ fundamentals/strings/ExampleStrings.java
@@ -0,0 +1,9 @@
+public class ExampleStrings {
+
+    static String example = "ALiteralString";
+
+    public static void main(String[] args) {
+        System.out.println(example.toLowerCase());
+    }
+}
+
