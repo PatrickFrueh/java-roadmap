ce16fca577806945fa738fcc34b06377a7d6e86c
Patrick F. <patrick.frueh@gmx.net>
2025-03-15 13:19:59 +0100

    Add iterator example

--- collections/Main.java
+++ collections/Main.java
@@ -1,8 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] intArray = new int[10];
-        int arrayLength = intArray.length;
-        System.out.println(arrayLength);
+        // int[] intArray = new int[10];
+        // int arrayLength = intArray.length;
+        // System.out.println(arrayLength);
+
+        // Create array of length 10
+        String[] stringArray = new String[10];
+
+            // Set the value at index "i" to the number of current index
+            for(int i=0; i < stringArray.length; i++) {
+                stringArray[i] = "String no " + i;
+            }
+
+            // Print each index of created array
+            for(int i=0; i < stringArray.length; i++) {
+                System.out.println( stringArray[i] );
+}
+
+
     }
 }
