e64f1e79ceaf56803410aa97f9f7fc04a7871c8a
Patrick F. <patrick.frueh@gmx.net>
2025-03-21 13:47:08 +0100

    Add 'matches'-example

--- regex/Regex.java
+++ regex/Regex.java
@@ -1,14 +1,19 @@
 import java.util.regex.*;
 
 class Regex {
-public static void main(String[] args) {
-    String text = "http://patrick.com";
+    public static void main(String[] args) {
+        String text = "http://patrick.com";
 
-    Pattern pattern = Pattern.compile("^http://");
-    Matcher matcher = pattern.matcher(text);
+        Pattern pattern = Pattern.compile("^http://");
+        Matcher matcher = pattern.matcher(text);
 
-    while(matcher.find()){
-        System.out.println("Found match at: "  + matcher.start() + " to " + matcher.end());
-}
+        while(matcher.find()){
+            System.out.println("Found match at: "  + matcher.start() + " to " + matcher.end());
+        }
+
+        String text_two = "one two three two one";
+
+        boolean matches = text_two.matches(".*two.*");
+        System.out.println(matches);
     }
 }
