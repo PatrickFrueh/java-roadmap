d7f5d81b2b8cd0e57dd8804788df035caf0dd1b7
Patrick F. <patrick.frueh@gmx.net>
2025-03-21 13:31:24 +0100

    Add carot example as anchor of beginning of string

---
+++ regex/Regex.java
@@ -0,0 +1,14 @@
+import java.util.regex.*;
+
+class Regex {
+public static void main(String[] args) {
+    String text = "http://patrick.com";
+
+    Pattern pattern = Pattern.compile("^http://");
+    Matcher matcher = pattern.matcher(text);
+
+    while(matcher.find()){
+        System.out.println("Found match at: "  + matcher.start() + " to " + matcher.end());
+}
+    }
+}
