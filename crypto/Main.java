17b4f289c4d628ca80508ce8758919d40c3ecf5c
Patrick F. <patrick.frueh@gmx.net>
2025-03-23 17:56:32 +0100

    Setup boilerplate

---
+++ crypto/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+        int number = 5;
+        String word = "Don't";
+
+        for (int i = 0; i <= number; i++) {
+            System.out.println(word + " judge!");
+
+        }
+    }
+}
