78d6787fcb99039b0c2cf2e07efc4262b66b6d3b
Patrick F. <patrick.frueh@gmx.net>
2025-03-23 18:33:39 +0100

    Add boilerplate security

--- crypto/Main.java
+++ crypto/Main.java
@@ -1,12 +1,12 @@
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 class Main {
 
     public static void main(String[] args) {
-        int number = 5;
-        String word = "Don't";
 
-        for (int i = 0; i <= number; i++) {
-            System.out.println(word + " judge!");
+        Security.addProvider(new BouncyCastleProvider());
 
-        }
     }
 }
